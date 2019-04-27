1ed107aec09afc57d14f59e83ca107d8621b5d4f
saori <kamachaso.30@gmail.com>
2019-04-27 13:11:03 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -17,15 +18,12 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to @task
     else
@@ -34,9 +32,14 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
   
   def task_params
